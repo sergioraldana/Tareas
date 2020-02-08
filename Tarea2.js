1f56b6df26eea326e8aabf29fee6350f9e0ea5a6
Renato Aldana <sergio.aldana@galileo.edu>
2020-02-07 20:54:17 -0600

    Agrega la solución de la Tarea 2

--- Tarea2.js
+++ Tarea2.js
@@ -1,1 +1,38 @@
 // JavaScript Document
+var mes = 10;
+
+var estacion = "";
+
+var nombreCompleto = "Sergio Renato Aldana Alvarez";
+
+var carne = "20001908";
+
+switch(mes) {
+	case 12:
+	case 1:
+	case 2:
+		var estacion = "Es invierno"
+		break;
+	case 3:
+	case 4:
+	case 5:
+		var estacion = "Es primavera"
+		break;
+	case 6:
+	case 7:
+	case 8:
+		var estacion = "Es verano"
+		break;
+	case 9:
+	case 10:
+	case 11:
+		var estacion = "Es otoÃ±o"
+		break;
+	default: 
+		var estacion = "No es un mes correcto"
+		break;
+}
+
+document.write(estacion, "<br>")
+document.write("Mi nombre es " + nombreCompleto, "<br>")
+document.write("Mi nÃºmero de carne es " + carne)
